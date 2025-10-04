a2b49279b6394c0a5225841768a5cc1d92c0d237
Super User <root@localhost.localdomain>
2025-10-04 12:37:35 -0500

    First commit

---
+++ dirbackup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This script backs up a directory of your choice
+echo -e "What directory do you want to backup? --> \c"
+read ANS
+echo "Performing backup....."
+sleep 3
+tar -zcvf ~/backupfile.tar.gz $ANS
+echo "Backup completed successfully to ~/backupfile.tar.gz"
